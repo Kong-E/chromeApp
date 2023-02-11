74a07c1927bcb92379a8f834343151adecf92381
Kong-E <kng001016@gmail.com>
2023-02-11 19:05:06 +0900

    bookmark add 기능 추가

--- js/bookmark.js
+++ js/bookmark.js
@@ -1,17 +1,61 @@
 const address = document.querySelector("#address");
 const text = document.querySelector("#bm-text");
+const addBtn = document.querySelector("#add-btn");
+const modalName = document.querySelector("#modal-name");
+const modalLink = document.querySelector("#modal-link");
+const saveBtn = document.querySelector("#save-btn");
+const closeBtn = document.querySelector("#close-btn");
 const bookmark = document.querySelector("#bookmark");
+const modal = document.querySelector("#modal");
 
 const HIDDEN = "hidden";
-const STYLE = "style";
+const BOOKMARK_KEY = "bookmark";
 
-address.addEventListener("mouseenter", () => {
+let bookmarks = [];
+
+function togg() {
   text.classList.toggle(HIDDEN);
   bookmark.classList.toggle(HIDDEN);
-  address.classList.toggle(STYLE);
-});
-address.addEventListener("mouseleave", () => {
-  text.classList.toggle(HIDDEN);
-  bookmark.classList.toggle(HIDDEN);
-  address.classList.toggle(STYLE);
-});
+}
+
+function modalToggle() {
+  modal.classList.toggle(HIDDEN);
+}
+
+function paintBookmark(bmObj) {
+  const a = document.createElement("a");
+  a.innerText = bmObj.name;
+  a.href = bmObj.link;
+  a.target = "_blank";
+  bookmark.prepend(a);
+}
+
+function submitBookmark(e) {
+  e.preventDefault();
+  const bmName = modalName.value;
+  const bmLink = modalLink.value;
+  modalName.value = "";
+  modalLink.value = "";
+  const bmObj = {
+    id: Date.now(),
+    name: bmName,
+    link: bmLink,
+  };
+  bookmarks.push(bmObj);
+  paintBookmark(bmObj);
+  localStorage.setItem(BOOKMARK_KEY, JSON.stringify(bookmarks));
+}
+
+address.addEventListener("mouseenter", togg);
+address.addEventListener("mouseleave", togg);
+addBtn.addEventListener("click", modalToggle);
+saveBtn.addEventListener("click", submitBookmark);
+closeBtn.addEventListener("click", modalToggle);
+
+const savedBookmarks = localStorage.getItem(BOOKMARK_KEY);
+
+if (savedBookmarks !== null) {
+  const parsedBookmarks = JSON.parse(savedBookmarks);
+  bookmarks = parsedBookmarks;
+  bookmarks.forEach(paintBookmark);
+}
--- js/todo.js
+++ js/todo.js
@@ -11,89 +11,93 @@ let toDos = [];
 // toDoRemove.addEventListener("click",deleteAllToDo);
 
 function changeOrder(e) {
-    const li = e.target.parentElement;
-    let idx;
-    // 바꿀요소의 인덱스 추출
-    for (var i=0; i<toDos.length; i++) {
-        if (toDos[i].ID === parseInt(li.id)) {
-            idx = i;
-            break;
-        }
+  const li = e.target.parentElement;
+  let idx;
+  // 바꿀요소의 인덱스 추출
+  for (var i = 0; i < toDos.length; i++) {
+    if (toDos[i].ID === parseInt(li.id)) {
+      idx = i;
+      break;
     }
-    if (idx === 0) return; 
-    const target = toDos.splice(idx, 1)[0];
-    toDos.splice(idx-1, 0, target);
-    deleteAllToDo();
-    toDos.forEach(paintToDo);
-    const lis = document.querySelectorAll("ul li");
-    for (var i=0; i<toDos.length; i++) {
-        if (toDos[i].done === true) {
-            lis[i].childNodes[0].classList.add(LINE_THROUGH);
-        }
+  }
+  if (idx === 0) return;
+  const target = toDos.splice(idx, 1)[0];
+  toDos.splice(idx - 1, 0, target);
+  deleteAllToDo();
+  toDos.forEach(paintToDo);
+  const lis = document.querySelectorAll("ul li");
+  for (var i = 0; i < toDos.length; i++) {
+    if (toDos[i].done === true) {
+      lis[i].childNodes[0].classList.add(LINE_THROUGH);
     }
-    saveToDos();
+  }
+  saveToDos();
 }
 
 function saveToDos() {
-    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function deleteToDo(e) {
-    const li = e.target.parentElement;
-    li.remove();
-    toDos = toDos.filter(item => item.ID !== parseInt(li.id));
-    saveToDos();
+  const li = e.target.parentElement;
+  li.remove();
+  toDos = toDos.filter((item) => item.ID !== parseInt(li.id));
+  saveToDos();
 }
 
 function deleteAllToDo() {
-    //임시
-    toDoList.innerHTML = ``;
+  //임시
+  toDoList.innerHTML = ``;
 }
 
 function addLineThrough(e) {
-    e.target.classList.toggle(LINE_THROUGH);
+  e.target.classList.toggle(LINE_THROUGH);
 
-    // 로컬스토리지 연동
-    const li = e.target.parentElement;
-    if (e.target.classList.contains(LINE_THROUGH)) {
-        toDos = toDos.map(item => item.ID === parseInt(li.id) ? {...item, done: true} : item);
-    } else {
-        toDos = toDos.map(item => item.ID === parseInt(li.id) ? {...item, done: false} : item);
-    }
-    saveToDos();
+  // 로컬스토리지 연동
+  const li = e.target.parentElement;
+  if (e.target.classList.contains(LINE_THROUGH)) {
+    toDos = toDos.map((item) =>
+      item.ID === parseInt(li.id) ? { ...item, done: true } : item
+    );
+  } else {
+    toDos = toDos.map((item) =>
+      item.ID === parseInt(li.id) ? { ...item, done: false } : item
+    );
+  }
+  saveToDos();
 }
 
 function paintToDo(newTodo) {
-    const li = document.createElement("li");
-    li.id = newTodo.ID;
-    const span = document.createElement("span");
-    span.innerText = newTodo.text;
-    span.addEventListener("click", addLineThrough);
-    const button1 = document.createElement("button");
-    button1.innerText = "X";
-    button1.addEventListener("click", deleteToDo);
-    const button2 = document.createElement("button");
-    button2.innerText = "▲";
-    button2.title = "위로 올려";
-    button2.addEventListener("click",changeOrder);
-    li.appendChild(span);
-    li.appendChild(button1);
-    li.appendChild(button2);
-    toDoList.appendChild(li);
+  const li = document.createElement("li");
+  li.id = newTodo.ID;
+  const span = document.createElement("span");
+  span.innerText = newTodo.text;
+  span.addEventListener("click", addLineThrough);
+  const button1 = document.createElement("button");
+  button1.innerText = "X";
+  button1.addEventListener("click", deleteToDo);
+  const button2 = document.createElement("button");
+  button2.innerText = "▲";
+  button2.title = "위로 올려";
+  button2.addEventListener("click", changeOrder);
+  li.appendChild(span);
+  li.appendChild(button1);
+  li.appendChild(button2);
+  toDoList.appendChild(li);
 }
 
 function handleToDoSubmit(e) {
-    e.preventDefault();
-    const newTodo = toDoInput.value;
-    toDoInput.value = "";
-    const newTodoObj = {
-        text: newTodo,
-        ID: Date.now(),
-        done: false,
-    }
-    toDos.push(newTodoObj);
-    paintToDo(newTodoObj);
-    saveToDos();
+  e.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  const newTodoObj = {
+    text: newTodo,
+    ID: Date.now(),
+    done: false,
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
@@ -101,13 +105,15 @@ toDoForm.addEventListener("submit", handleToDoSubmit);
 const savedToDos = localStorage.getItem(TODOS_KEY);
 
 if (savedToDos !== null) {
-    const parsedToDos = JSON.parse(savedToDos);
-    toDos = parsedToDos;
-    toDos.forEach(paintToDo);
-    const lis = document.querySelectorAll("ul li");
-    for (var i=0; i<toDos.length; i++) {
-        if (toDos[i].done === true) {
-            lis[i].childNodes[0].classList.add(LINE_THROUGH);
-        }
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  toDos.forEach(paintToDo);
+
+  // 라인 추가
+  const lis = document.querySelectorAll("ul li");
+  for (var i = 0; i < toDos.length; i++) {
+    if (toDos[i].done === true) {
+      lis[i].childNodes[0].classList.add(LINE_THROUGH);
     }
+  }
 }
