f3e7b0d98cecd5378ac0e6727620ead8fa84895e
Kong-E <kng001016@gmail.com>
2023-03-01 15:58:48 +0900

    북마크 삭제 기능 & 드래그앤드랍 로컬스토리지 반영

--- js/bookmark.js
+++ js/bookmark.js
@@ -1,17 +1,22 @@
 const address = document.querySelector("#address");
 const text = document.querySelector("#bm-text");
+const bookmark = document.querySelector("#bookmark");
 const addBtn = document.querySelector("#add-btn");
+const editBtn = document.querySelector("#edit-btn");
 const modal = document.querySelector("#modal");
 const modalName = document.querySelector("#modal-name");
 const modalLink = document.querySelector("#modal-link");
 const saveBtn = document.querySelector("#save-btn");
 const closeBtn = document.querySelector("#close-btn");
-const bookmark = document.querySelector("#bookmark");
 
 const HIDDEN = "hidden";
 const BOOKMARK_KEY = "bookmark";
 
 let bookmarks = [];
+
+function saveBookmarks() {
+  localStorage.setItem(BOOKMARK_KEY, JSON.stringify(bookmarks));
+}
 
 function enterToggle() {
   text.classList.add(HIDDEN);
@@ -31,11 +36,19 @@ function modalToggle() {
 }
 
 function paintBookmark(bmObj) {
+  const div = document.createElement("div");
+  div.id = bmObj.ID;
   const a = document.createElement("a");
   a.innerText = bmObj.name;
   a.href = bmObj.link;
   a.target = "_blank";
-  bookmark.prepend(a);
+  const btn = document.createElement("button");
+  btn.innerHTML = '<i class="fa-regular fa-x"></i>';
+  btn.id = "del-btn";
+  btn.classList.add(HIDDEN);
+  div.append(a);
+  div.append(btn);
+  bookmark.prepend(div);
 }
 
 function submitBookmark(e) {
@@ -46,21 +59,32 @@ function submitBookmark(e) {
   modalName.value = "";
   modalLink.value = "https://";
   const bmObj = {
-    id: Date.now(),
+    ID: Date.now(),
     name: bmName,
     link: bmLink,
   };
   bookmarks.push(bmObj);
   paintBookmark(bmObj);
-  localStorage.setItem(BOOKMARK_KEY, JSON.stringify(bookmarks));
+  saveBookmarks();
 }
 
 address.addEventListener("mouseenter", enterToggle);
 address.addEventListener("mouseleave", leaveToggle);
-modal.addEventListener("mouseleave", leaveToggle);
 addBtn.addEventListener("click", modalToggle);
 saveBtn.addEventListener("click", submitBookmark);
 closeBtn.addEventListener("click", modalToggle);
+editBtn.addEventListener("click", () => {
+  const deleteBtns = document.querySelectorAll("#del-btn");
+  deleteBtns.forEach((btn) => {
+    btn.classList.toggle(HIDDEN);
+    btn.addEventListener("click", (e) => {
+      const div = e.target.parentElement.parentElement;
+      div.remove();
+      bookmarks = bookmarks.filter((item) => item.ID !== parseInt(div.id));
+      saveBookmarks();
+    });
+  });
+});
 
 const savedBookmarks = localStorage.getItem(BOOKMARK_KEY);
 
--- js/todo.js
+++ js/todo.js
@@ -11,6 +11,32 @@ let toDos = [];
 
 // toDoRemove.addEventListener("click",deleteAllToDo);
 
+function addLineThrough(e) {
+  e.target.classList.toggle(LINE_THROUGH);
+
+  // 로컬스토리지 연동
+  const li = e.target.parentElement;
+  if (e.target.classList.contains(LINE_THROUGH)) {
+    toDos = toDos.map((item) =>
+      item.ID === parseInt(li.id) ? { ...item, done: true } : item
+    );
+  } else {
+    toDos = toDos.map((item) =>
+      item.ID === parseInt(li.id) ? { ...item, done: false } : item
+    );
+  }
+  saveToDos();
+}
+
+function paintLineThrough() {
+  const lis = document.querySelectorAll("ul li");
+  for (var i = 0; i < toDos.length; i++) {
+    if (toDos[i].done === true) {
+      lis[i].childNodes[0].classList.add(LINE_THROUGH);
+    }
+  }
+}
+
 function changeOrder(e) {
   const li = e.target.parentElement.parentElement;
   let idx;
@@ -26,12 +52,7 @@ function changeOrder(e) {
   toDos.splice(idx - 1, 0, target);
   deleteAllToDo();
   toDos.forEach(paintToDo);
-  const lis = document.querySelectorAll("ul li");
-  for (var i = 0; i < toDos.length; i++) {
-    if (toDos[i].done === true) {
-      lis[i].childNodes[0].classList.add(LINE_THROUGH);
-    }
-  }
+  paintLineThrough();
   saveToDos();
 }
 
@@ -51,30 +72,13 @@ function deleteAllToDo() {
   toDoList.innerHTML = ``;
 }
 
-function addLineThrough(e) {
-  e.target.classList.toggle(LINE_THROUGH);
-
-  // 로컬스토리지 연동
-  const li = e.target.parentElement;
-  if (e.target.classList.contains(LINE_THROUGH)) {
-    toDos = toDos.map((item) =>
-      item.ID === parseInt(li.id) ? { ...item, done: true } : item
-    );
-  } else {
-    toDos = toDos.map((item) =>
-      item.ID === parseInt(li.id) ? { ...item, done: false } : item
-    );
-  }
-  saveToDos();
-}
-
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   li.id = newTodo.ID;
   li.draggable = true;
-  const span = document.createElement("span");
-  span.innerText = newTodo.text;
-  span.addEventListener("click", addLineThrough);
+  const div = document.createElement("div");
+  div.innerText = newTodo.text;
+  div.addEventListener("click", addLineThrough);
   const button1 = document.createElement("button");
   button1.innerHTML = '<span class="fa-solid fa-x"></span>';
   button1.children[0].addEventListener("click", deleteToDo);
@@ -82,10 +86,11 @@ function paintToDo(newTodo) {
   button2.innerHTML = '<span class="fa-solid fa-caret-up"></span>';
   button2.title = "위로 올리기";
   button2.children[0].addEventListener("click", changeOrder);
-  li.appendChild(span);
+  li.appendChild(div);
   li.appendChild(button1);
   li.appendChild(button2);
   toDoList.appendChild(li);
+  addEventsDragAndDrop(li);
 }
 
 function handleToDoSubmit(e) {
@@ -112,46 +117,81 @@ if (savedToDos !== null) {
   toDos.forEach(paintToDo);
 
   // 라인 추가
-  const lis = document.querySelectorAll("ul li");
-  for (var i = 0; i < toDos.length; i++) {
-    if (toDos[i].done === true) {
-      lis[i].childNodes[0].classList.add(LINE_THROUGH);
-    }
-  }
+  paintLineThrough();
 }
 
-function dragStart(e) {
+// 드래그 시작할 때 1번 호출
+async function dragStart(e) {
   console.log("drag start");
+  console.log(e.target.id, e.target.innerText);
   this.style.opacity = "0.4";
   dragSrcEl = this;
   e.dataTransfer.effectAllowed = "move";
   e.dataTransfer.setData("text/html", this.innerHTML);
 }
 
-function dragEnter(e) {
+// 마우스가 요소로 진입할 때 1번 호출
+async function dragEnter(e) {
+  console.log("drag enter");
+  console.log(e.target.id, e.target.innerText);
   this.classList.add("over");
 }
 
+// 다른 요소로 넘어갈 때 1번 호출
 function dragLeave(e) {
   e.stopPropagation();
+  console.log("drag leave");
+  console.log(e.target.id, e.target.innerText);
   this.classList.remove("over");
 }
 
+// 마우스 누른 채 유지할 때 계속 호출
 function dragOver(e) {
   e.preventDefault();
+  console.log("drag over");
+  console.log(e.target.id, e.target.innerText);
   e.dataTransfer.dropEffect = "move";
   return false;
 }
 
+// 다른 요소로 옮기고 드랍했을 때 1번 호출
 function dragDrop(e) {
   if (dragSrcEl != this) {
+    console.log("drag drop");
+    /* this와 e.target은 할일2 */
+    //드래그 시작 요소 html = 현재 요소 html
     dragSrcEl.innerHTML = this.innerHTML;
+    // 현재 요소 html = 드래그 시작 요소 html
     this.innerHTML = e.dataTransfer.getData("text/html");
+
+    //parseInt(dragSrcEl.id) === toDo.ID, parseInt(this.id) === toDo.ID 찾아서 서로 자리 교환한후 saveToDos()
+    const toDo1 = toDos.find((toDo) => toDo.ID === parseInt(dragSrcEl.id));
+    const toDo2 = toDos.find((toDo) => toDo.ID === parseInt(this.id));
+
+    let toDo1_Idx;
+    let toDo2_Idx;
+
+    for (let i = 0; i < toDos.length; i++) {
+      if (toDos[i].ID === parseInt(dragSrcEl.id)) toDo1_Idx = i;
+      if (toDos[i].ID === parseInt(this.id)) toDo2_Idx = i;
+    }
+
+    toDos.splice(toDo1_Idx, 1, toDo2);
+    toDos.splice(toDo2_Idx, 1, toDo1);
+    console.log(toDos);
+
+    deleteAllToDo();
+    toDos.forEach(paintToDo);
+    paintLineThrough();
+    saveToDos();
   }
   return false;
 }
 
+// 마우스업 하면 1번 호출
 function dragEnd(e) {
+  console.log("drag end");
+  console.log(e.target.id, e.target.innerText);
   let lis = document.querySelectorAll("ul li");
   [].forEach.call(lis, function (item) {
     item.classList.remove("over");
@@ -168,7 +208,7 @@ function addEventsDragAndDrop(el) {
   el.addEventListener("dragend", dragEnd, false);
 }
 
-let lis = document.querySelectorAll("ul li");
+/* let lis = document.querySelectorAll("ul li");
 [].forEach.call(lis, function (item) {
   addEventsDragAndDrop(item);
-});
+}); */
